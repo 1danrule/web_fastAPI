01de63ad94cdea9c5c2ed88f2b5ab11d1b2da642
Kolor <Kol@email.com>
2024-12-08 19:52:57 +0100

    some changes in search params

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def index():
     return {'status': 200}
 
 
-@app.get('/offer/details')
+@app.get('/tours/{tour_id}/details')
 def offer_details(information: str = Query("default_info", title='Additional details about the offer',
                                            description='Provide details like tour location or special requests')):
     return {
@@ -19,8 +19,8 @@ def offer_details(information: str = Query("default_info", title='Additional det
     }
 
 
-@app.get('/offer/')
-def with_query_param(tour_type: str = Query(title='Type of tour (e.g., adventure, beach, cultural)', default='Write')):
+@app.get('/tours/')
+def with_query_param(tour_type: str = Query(title='Type of tour (e.g., operator, country, duration)', default='Write')):
     return {
         'some_information': tour_type,
         'message': f'Filtering tours by type: {tour_type}'
